3ff65059b7414a4f85336e1bc8d561c662d03cea
Koji Shinba <bojovs@gmail.com>
2012-09-01 02:06:57 +0900

    hello world

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+# Assets
+gem 'jquery-rails', '2.1.1'
+
+# Controllers
+
+# Models
+gem 'mysql2', '0.3.11'
+
+# Views
+
+
+group :assets do
+  gem 'coffee-rails', '3.2.2'
+  gem 'sass-rails', '3.2.5'
+  gem 'uglifier', '1.2.7'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startrip::Application.config.secret_token = '935a36f2930c19eaac0865afef0f01b63367b2e071a1cfea234987e19d0fc94529758aee0b5d090898bd70619744d7cb61c74896220a8e3296289bee6f0833aa'
