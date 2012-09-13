bb86b2d07f2a987c9737d0600b51c525263f31f5
Koji Shinba <bojovs@gmail.com>
2012-09-14 06:35:24 +0900

    ページネーション処理の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user).order('created_at DESC')
+    @posts = Post.includes(:user).order('created_at DESC').page(params[:page])
   end
 end
